0879295959e7a988de13c4400c279a6c07881a69
Krist Phan <phanthithuthao0204@gmail.com>
2024-08-05 16:01:57 +0700

    Mô tả thay đổi

--- backend/src/models/booking.ts
+++ backend/src/models/booking.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import Vet from "./vet";
 
 const bookingSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
-    vetId: { type: String, ref: "Vet", required: true },
+    vetId: { type: String, ref: Vet, required: true },
     ownerId: { type: String, ref: "Owner", required: false },
     petId: { type: String, ref: "Pet", required: false },
     phoneOwner: { type: String, required: true },
