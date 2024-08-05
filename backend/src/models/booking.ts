68bd70261952ba59efc48aafb5f388b40414125b
tvinhthanh <tvinhthanhsg@gmail.com>
2024-08-05 18:14:55 +0700

    nice

--- backend/src/models/booking.ts
+++ backend/src/models/booking.ts
@@ -1,22 +1,14 @@
 import mongoose from "mongoose";
 import Vet from "./vet";
-<<<<<<< HEAD
-=======
 import Owner from "./owner";
 import Pet from "./pet";
->>>>>>> 2d725c15 (5.8 remove controller)
 
 const bookingSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
     vetId: { type: String, ref: Vet, required: true },
-<<<<<<< HEAD
-    ownerId: { type: String, ref: "Owner", required: false },
-    petId: { type: String, ref: "Pet", required: false },
-=======
     ownerId: { type: String, ref: Owner, required: false },
     petId: { type: String, ref: Pet, required: false },
->>>>>>> 2d725c15 (5.8 remove controller)
     phoneOwner: { type: String, required: true },
     date: { type: Date, required: true },
     status: { type: Number, required: true },
