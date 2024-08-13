725870c7ecf2fd8f0dfacfc6b167bd99f49d3331
Krist Phan <phanthithuthao0204@gmail.com>
2024-08-14 04:04:23 +0700

    add review model

---
+++ backend/src/models/review.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import UsersApp from "./usersApp";
+import Vet from "./vet";
+
+
+const reviewSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    vetId: { type: String, ref: Vet, required: false },
+    ownerId: { type: String, ref: UsersApp, required: false },
+    comment: { type: String, required: false },
+    rating: { type: Number, required: false },
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("review", reviewSchema);
+
+export default Booking;
