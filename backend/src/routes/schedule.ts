660b02f5e8f2fc353d8738dcd30a6fffd3f332f1
Krist Phan <phanthithuthao0204@gmail.com>
2024-08-13 19:41:28 +0700

    update

--- backend/src/routes/schedule.ts
+++ backend/src/routes/schedule.ts
@@ -28,4 +28,93 @@ router.get('/appointment/:email', async (req, res) => {
   }
 });
 
+// Endpoint thêm sự kiện mới
+router.post('/events/add/:email', async (req, res) => {
+  const { email } = req.params;
+  const { title, description, datetime } = req.body; // Removed booking_id
+
+  try {
+    // Find the user by email
+    const user = await UsersApp.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Create a new schedule with type set to "E"
+    const newSchedule = new Schedule({
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      owner_id: user._id,
+      title,
+      description,
+      datetime,
+      type: "E"  // Set the type to "E"
+    });
+
+    // Save the new schedule
+    await newSchedule.save();
+
+    res.status(201).json({ message: 'New schedule created', schedule: newSchedule });
+  } catch (error) {
+    console.error('Error creating schedule:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//Endpoint update sự kiện
+router.put('/events/update/:email/:scheduleId', async (req, res) => {
+  const { email, scheduleId } = req.params;
+  const { title, description, datetime } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await UsersApp.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Find and update the schedule
+    const updatedSchedule = await Schedule.findOneAndUpdate(
+      { _id: scheduleId, owner_id: user._id },
+      { title, description, datetime },
+      { new: true }
+    );
+
+    if (!updatedSchedule) {
+      return res.status(404).json({ message: 'Schedule not found' });
+    }
+
+    res.status(200).json(updatedSchedule);
+  } catch (error) {
+    console.error('Error updating schedule:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+//Endpoint delete sự kiện
+router.delete('/events/delete/:email/:scheduleId', async (req, res) => {
+  const { email, scheduleId } = req.params;
+
+  try {
+    // Find the user by email
+    const user = await UsersApp.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Find and delete the schedule
+    const deletedSchedule = await Schedule.findOneAndDelete({
+      _id: scheduleId,
+      owner_id: user._id
+    });
+
+    if (!deletedSchedule) {
+      return res.status(404).json({ message: 'Schedule not found' });
+    }
+
+    res.status(200).json({ message: 'Schedule deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting schedule:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 export default router;
--- flutter_petcare_app/lib/features/auth/presentation/pages/calendar/calendar_page.dart
+++ flutter_petcare_app/lib/features/auth/presentation/pages/calendar/calendar_page.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter_petcare_app/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_petcare_app/core/theme/app_pallete.dart';
 import 'package:flutter_petcare_app/features/auth/presentation/pages/loginSignup/login_page.dart';
 import 'package:flutter_petcare_app/features/auth/presentation/widgets/custom_drawer.dart';
@@ -12,7 +12,9 @@ class CalendarPage extends StatefulWidget {
   final String? userName;
   final String? email;
   final String? imageURLs;
-  const CalendarPage({Key? key, this.userName, this.email, this.imageURLs}) : super(key: key);
+
+  const CalendarPage({Key? key, this.userName, this.email, this.imageURLs})
+      : super(key: key);
 
   @override
   State<CalendarPage> createState() => _CalendarPageState();
@@ -20,150 +22,14 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  late final ValueNotifier<List<Event>> _selectedEvents;
-
-  final Map<DateTime, List<Event>> _events = {};
 
   @override
   void initState() {
     super.initState();
     _selectedDay = _focusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
-    _fetchAppointments(); // Fetch appointments on initialization
-  }
-
-  @override
-  void dispose() {
-    _selectedEvents.dispose();
-    super.dispose();
-  }
-
-  Future<String> _getUserIdFromEmail(String email) async {
-    final String url =
-        'http://${Ip.serverIP}:3000/api/usersApp/userInfo/userId/$email'; // Create an endpoint in your backend to fetch user by email
-
-    try {
-      final response = await http.get(Uri.parse(url));
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        return data['_id']; // Adjust according to the structure of the response
-      } else {
-        throw Exception('Failed to load user: ${response.reasonPhrase}');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Error fetching user ID: $e')));
-      return ''; // Return an empty string or handle it as you wish
-    }
-  }
-
-  Future<void> _fetchAppointments() async {
-    if (widget.email == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Email is null')),
-      );
-      return; // Exit early if email is null
-    }
-
-    final String email = widget.email!;
-    String userId = await _getUserIdFromEmail(email);
-    if (userId.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('UserId is null')),
-      );
-      return;
-    }
-
-    try {
-      final response = await http.get(Uri.parse(
-          'http://${Ip.serverIP}:3000/api/schedule/appointments/calendar/$email'));
-
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        for (var appointment in data) {
-          String title = appointment['vetId']['name'];
-          DateTime eventDate = DateTime.parse(appointment['date']).toLocal();
-          String time = TimeOfDay.fromDateTime(eventDate).format(context);
-          String description = appointment['note'] ?? '';
-          String bookingId = appointment['_id']; // Extract booking_id
-          String type = 'LH'; // Extract type
-
-          DateTime normalizedDate = _normalizeDate(eventDate);
-
-          // Check if the event already exists in _events based on booking_id
-          bool eventExists = _events[normalizedDate]?.any((event) =>
-                  event.type == type &&
-                  event.title == title &&
-                  event.description == description &&
-                  appointment['_id'] == bookingId) ??
-              false;
-
-          if (!eventExists) {
-            // Add event to the _events map
-            _events.putIfAbsent(normalizedDate, () => []).add(
-                  Event(title, time, description, type),
-                );
-
-            // Save the appointment to the backend
-            await _saveAppointmentToBackend(userId, bookingId, title,
-                description, eventDate, type); // Pass userId here
-          } else {
-            // Nếu sự kiện đã tồn tại, bạn có thể thêm một thông báo hoặc xử lý khác ở đây
-            print('Event with booking_id $bookingId already exists.');
-          }
-        }
-        // Update the selected events after fetching
-        _selectedEvents.value = _getEventsForDay(_selectedDay!);
-      } else {
-        throw Exception(
-            'Failed to load appointments: ${response.reasonPhrase}');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Error: $e')));
-    }
-  }
-
-  Future<void> _saveAppointmentToBackend(String userId, String bookingId,
-      String title, String description, DateTime eventDate, String type) async {
-    final String url =
-        'http://${Ip.serverIP}:3000/api/schedule/addEvent'; // Replace with your actual endpoint
-    final body = json.encode({
-      'owner_id': userId,
-      'booking_id': bookingId,
-      'title': title,
-      'description': description,
-      'datetime': eventDate.toIso8601String(),
-      'type': type,
-    });
-
-    try {
-      final response = await http.post(
-        Uri.parse(url),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: body,
-      );
-
-      if (response.statusCode != 201) {
-        throw Exception('Failed to save appointment: ${response.reasonPhrase}');
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Error saving appointment: $e')));
-    }
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    return _events[_normalizeDate(day)] ?? [];
-  }
-
-  DateTime _normalizeDate(DateTime date) {
-    return DateTime.utc(date.year, date.month, date.day);
   }
 
   Future<void> _showLogoutConfirmationDialog(BuildContext context) async {
@@ -203,48 +69,25 @@ class _CalendarPageState extends State<CalendarPage> {
     );
   }
 
-  Future<void> _showAddEventDialog(BuildContext context) async {
-    String title = '';
-    String time = '';
-    String description = '';
-
-    TextEditingController timeController = TextEditingController();
+  Future<void> _addEvent(BuildContext context) async {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
 
     return showDialog<void>(
       context: context,
-      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Thêm sự kiện'),
+          title: Text('Add Event'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
+            children: [
               TextField(
-                decoration: InputDecoration(labelText: 'Tiêu đề: '),
-                onChanged: (value) {
-                  title = value;
-                },
+                controller: titleController,
+                decoration: InputDecoration(labelText: 'Event Title'),
               ),
               TextField(
-                readOnly: true,
-                decoration: InputDecoration(labelText: 'Chọn thời gian: '),
-                controller: timeController,
-                onTap: () async {
-                  TimeOfDay? pickedTime = await showTimePicker(
-                    context: context,
-                    initialTime: TimeOfDay.now(),
-                  );
-                  if (pickedTime != null) {
-                    time = pickedTime.format(context);
-                    timeController.text = time;
-                  }
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Mô tả:'),
-                onChanged: (value) {
-                  description = value;
-                },
+                controller: descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
               ),
             ],
           ),
@@ -256,128 +99,17 @@ class _CalendarPageState extends State<CalendarPage> {
               },
             ),
             TextButton(
-              child: Text('Confirm'),
+              child: Text('Add'),
               onPressed: () {
-                setState(() {
-                  final normalizedDate = _normalizeDate(_selectedDay!);
-                  _events.putIfAbsent(normalizedDate, () => []).add(
-                      Event(title, time, description, '')); // Set default type
-                  _selectedEvents.value = _getEventsForDay(_selectedDay!);
-                });
+                // Handle event addition logic here
+                String title = titleController.text;
+                String description = descriptionController.text;
+                // You can save the event to a database or list here
                 Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _showEditDeleteDialog(BuildContext context, Event event,
-      {required DateTime normalizedDate, required int eventIndex}) async {
-    if (event.type == 'LH') {
-      // If the type is 'LH', show only event details
-      return showDialog<void>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Chi tiết lịch hẹn'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment:
-                  CrossAxisAlignment.start, // Align content to the start (left)
-              children: <Widget>[
-                Text('Phòng khám: ${event.title}'),
-                Text('Thời gian: ${event.time}'),
-                Text('Mô tả: ${event.description}'),
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Close'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-    // For other types, show edit/delete options
-    String newTitle = event.title;
-    String newTime = event.time;
-    String newDescription = event.description;
-
-    TextEditingController timeController = TextEditingController(text: newTime);
-
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Thông tin sự kiện'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'Tiêu đề:'),
-                controller: TextEditingController(text: newTitle),
-                onChanged: (value) {
-                  newTitle = value;
-                },
-              ),
-              TextField(
-                readOnly: true,
-                decoration: InputDecoration(labelText: 'Thời gian:'),
-                controller: timeController,
-                onTap: () async {
-                  TimeOfDay? pickedTime = await showTimePicker(
-                    context: context,
-                    initialTime: TimeOfDay.now(),
-                  );
-                  if (pickedTime != null) {
-                    newTime = pickedTime.format(context);
-                    timeController.text = newTime;
-                  }
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Mô tả:'),
-                controller: TextEditingController(text: newDescription),
-                onChanged: (value) {
-                  newDescription = value;
-                },
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Xóa sự kiện'),
-              onPressed: () {
-                setState(() {
-                  _events[normalizedDate]!.removeAt(eventIndex);
-                  _selectedEvents.value = _getEventsForDay(_selectedDay!);
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Cập nhật'),
-              onPressed: () {
-                setState(() {
-                  _events[normalizedDate]![eventIndex] =
-                      Event(newTitle, newTime, newDescription, event.type);
-                  _selectedEvents.value = _getEventsForDay(_selectedDay!);
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Hủy'),
-              onPressed: () {
-                Navigator.of(context).pop();
+                // Optionally show a message
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Event "$title" added!')),
+                );
               },
             ),
           ],
@@ -418,110 +150,44 @@ class _CalendarPageState extends State<CalendarPage> {
       drawer: CustomDrawer(
         userName: widget.userName,
         email: widget.email,
-        imageURLs: widget.imageURLs
+        imageURLs: widget.imageURLs,
       ),
       body: Column(
         children: [
-          TableCalendar<Event>(
-            firstDay: DateTime.now().subtract(Duration(days: 365)),
-            lastDay: DateTime.now().add(Duration(days: 365)),
+          TableCalendar(
+            firstDay:
+                DateTime.utc(2020, 1, 1), // Change the start date as needed
+            lastDay:
+                DateTime.utc(2030, 12, 31), // Change the end date as needed
             focusedDay: _focusedDay,
             selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
             onDaySelected: (selectedDay, focusedDay) {
               if (!isSameDay(_selectedDay, selectedDay)) {
                 setState(() {
                   _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                  _selectedEvents.value = _getEventsForDay(selectedDay);
+                  _focusedDay = focusedDay; // Update `_focusedDay` here as well
                 });
               }
             },
-            eventLoader: _getEventsForDay,
-            onFormatChanged: (format) {},
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
             onPageChanged: (focusedDay) {
               _focusedDay = focusedDay;
             },
-            // Customizing the calendar builders
-            calendarBuilders: CalendarBuilders(
-              // Customize the day builder
-              todayBuilder: (context, day, focusedDay) {
-                return Container(
-                  margin: const EdgeInsets.all(4.0),
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent, // Color for today
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Center(
-                    child: Text(
-                      '${day.day}',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                );
-              },
-              // Custom builder for days with events
-              markerBuilder: (context, day, events) {
-                if (events.isNotEmpty) {
-                  return Container(
-                    margin: const EdgeInsets.all(4.0),
-                    decoration: BoxDecoration(
-                      color: Colors.red, // Change the color here
-                      shape: BoxShape.circle,
-                    ),
-                    width: 8.0,
-                    height: 8.0,
-                  );
-                }
-                return SizedBox(); // Return an empty widget for days without events
-              },
-            ),
           ),
-          const SizedBox(height: 8.0),
-          ValueListenableBuilder<List<Event>>(
-            valueListenable: _selectedEvents,
-            builder: (context, value, _) {
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    final event = value[index];
-                    return ListTile(
-                      title: Text("Tiêu đề: " + event.title),
-                      subtitle: Text("Thời gian: " +
-                          event.time +
-                          "\nMô tả: " +
-                          event.description),
-                      onTap: () {
-                        final normalizedDate = _normalizeDate(_selectedDay!);
-                        final eventIndex =
-                            _events[normalizedDate]!.indexOf(event);
-                        _showEditDeleteDialog(context, event,
-                            normalizedDate: normalizedDate,
-                            eventIndex: eventIndex);
-                      },
-                    );
-                  },
-                ),
-              );
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              _addEvent(context);
             },
+            child: Text('Add Event'),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddEventDialog(context);
-        },
-        child: Icon(Icons.add),
-      ),
     );
   }
-}
-
-class Event {
-  final String title;
-  final String time;
-  final String description;
-  final String type;
-
-  Event(this.title, this.time, this.description, this.type);
 }
